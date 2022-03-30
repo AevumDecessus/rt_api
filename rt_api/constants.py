c95dd86f1f1d52ab7e30703b047b51c3af173453
AevumDecessus <aevum@decess.us>
2022-03-30 16:02:24 -0400

    Old API endpoint returns HTTP 503

--- rt_api/constants.py
+++ rt_api/constants.py
@@ -1,4 +1,4 @@
-END_POINT = "https://roosterteeth.com/api/v1/"
+END_POINT = "https://svod-be.roosterteeth.com/api/v1/"
 AUTH_URL = "https://auth.roosterteeth.com/oauth/token"
 CLIENT_ID = "4338d2b4bdc8db1239360f28e72f0d9ddb1fd01e7a38fbb07b4b1f4ba4564cc5"
 CLIENT_SECRET = ""
